6f8c4955899a8f4f6cb85a2ca1b3cc1325279823
Sobiah Selvarajah <sobiahselvarajah@hotmail.co.uk>
2022-08-14 16:02:03 +0100

    passwordchecker.py - func to get password leaks count added

--- passwordchecker.py
+++ passwordchecker.py
@@ -12,21 +12,28 @@ def request_api_data(query):
     return res
 
 
-def read_response(response):
-    print(response.text)
+# func - get the number of password leaks from the responses that match the first 5 characters
+# param1 - list of response hashes matching first 5 characters
+# param2 - my hashed password
+def password_leaks_count(response_hashes, my_password_hash):
+    response_hashes = (each_hash.split(':') for each_hash in response_hashes.text.splitlines())
+    for res_hash, count in response_hashes:
+        if res_hash == my_password_hash:
+            return count
+        return 0
 
 
 # func - checks existence of password in API response using sha1 algo
 # param - our actual password
 # response - all the hashed passwords that match the first 5 characters of my password
-def pwned_api_response_check(password):
-    sha1pass = hashlib.sha1(password.encode('utf-8')).hexdigest().upper()
+def pwned_api_response_check(my_password):
+    sha1pass = hashlib.sha1(my_password.encode('utf-8')).hexdigest().upper()
     first5char = sha1pass[:5]
     tail = sha1pass[5:]
     response = request_api_data(first5char)
     print(first5char, tail)
     print(response)
-    return read_response(response)
+    return password_leaks_count(response, tail)
 
 
 pwned_api_response_check('123')
